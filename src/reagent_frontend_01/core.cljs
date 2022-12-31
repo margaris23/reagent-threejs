ba668a52f6bc64fb06132f7c3fd3f8d45b6aa431
reverence23@gmail.com <nma@anixe.io>
2022-12-31 19:08:50 +0200

    minor code clean up, inline def

--- src/reagent_frontend_01/core.cljs
+++ src/reagent_frontend_01/core.cljs
@@ -28,10 +28,38 @@
 (defn formula [a b]
   (-> (/ a b) (* 2) (- 1)))
 
+(defn onPointerMove [pointer]
+  (fn [event]
+    (set! (.-x pointer) (formula (.-clientX event) (.-innerWidth js/window)))
+    (set! (.-y pointer) (formula (.-clientY event) (.-innerHeight js/window)))))
+
+(defn onWindowResize [renderer ^js camera]
+  (fn []
+    (set! (.-aspect camera) (screen-ratio))
+    (.updateProjectionMatrix camera)
+    (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))))
+
+(def intersected (atom nil))
+
+(defn update-intersection! [intersects]
+  #_(js/console.log (.-length intersects))
+  #_(js/console.log pointer)
+  (if (> (.-length intersects) 0)
+    ; if-case
+    (when (not (= intersected (.-object (aget intersects 0))))
+      (when-let [^js intersected @intersected]
+        (set! (.. intersected -material -color) (.. intersected -currentColor)))
+      (reset! intersected (.-object (aget intersects 0)))
+      (set! (.-currentColor ^js @intersected) (.. @intersected -material -color))
+      (set! (.. ^js @intersected -material -color) (t/Color. 0x777777)))
+    ; else-case
+    (do
+      (when @intersected
+        (set! (.. @intersected -material -color) (.-currentColor ^js @intersected)))
+      (reset! intersected nil))))
 
 ;; -------------------------
 ;; Components
-(def intersected (atom nil))
 
 (defn MyScene []
   (let [scene (t/Scene.)
@@ -46,78 +74,47 @@
         controls (toc/OrbitControls. camera (.-domElement renderer))
         ; controls (tfc/FlyControls. camera (.-domElement renderer))
         !my-ref (react/useRef)]
-    ; pointerMove function
-    (defn onPointerMove [event]
-      (do
-        (set! (.-x pointer) (formula (.-clientX event) (.-innerWidth js/window)))
-        (set! (.-y pointer) (formula (.-clientY event) (.-innerHeight js/window)))))
-    ; window resize function
-    (defn onWindowResize []
-      (set! (.-aspect camera) (screen-ratio))
-      (.updateProjectionMatrix camera)
-      (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window)))
-    ; update intersection object with pointer by changing color
-    (defn update-intersection! [intersects]
-        #_(js/console.log (.-length intersects))
-        #_(js/console.log pointer)
-        (if (> (.-length intersects) 0)
-          ; if-case
-          (if (not (= intersected (.-object (aget intersects 0))))
-            (do
-              (if-let [^js intersected @intersected]
-                (set! (.. intersected -material -color) (.. intersected -currentColor)))
-              (reset! intersected (.-object (aget intersects 0)))
-              (set! (.-currentColor ^js @intersected) (.. @intersected -material -color))
-              (set! (.. ^js @intersected -material -color) (t/Color. 0x777777))))
-          ; else-case
-          (do
-            (if @intersected
-              (set! (.. @intersected -material -color) (.-currentColor ^js @intersected)))
-            (reset! intersected nil))))
     ; custom animation function
     (defn animate []
       (let [delta (.getDelta clock)]
-        (do
-          (rotateBox cube)
-          (rotateBox cube2)
-          (rotateBox cube3)
-          (.update controls delta)
-          (.setFromCamera raycaster pointer camera)
-          (update-intersection! (.intersectObjects raycaster (.-children scene) false))
-          (.render renderer scene camera)
-          (set! (.-current !my-ref) (js/requestAnimationFrame animate)))))
+        (rotateBox cube)
+        (rotateBox cube2)
+        (rotateBox cube3)
+        (.update controls delta)
+        (.setFromCamera raycaster pointer camera)
+        (update-intersection! (.intersectObjects raycaster (.-children scene) false))
+        (.render renderer scene camera)
+        (set! (.-current !my-ref) (js/requestAnimationFrame animate))))
     ; effect (hook) runs only once
     (react/useEffect (fn []
-       (do
-         (set! (.-current !my-ref) (js/requestAnimationFrame animate))
-         #(js/cancelAnimationFrame (.-current !my-ref))))
-       (array)) ; array is used to run effect only once
+                       (set! (.-current !my-ref) (js/requestAnimationFrame animate))
+                       #(js/cancelAnimationFrame (.-current !my-ref)))
+                     (array)) ; array is used to run effect only once
     ; steps to generate result
-    (do
-      (.setPixelRatio renderer (.-devicePixelRatio js/window)) ; possibly not needed
-      (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
-      (.appendChild (.-body js/document) (.-domElement renderer))
-      ; add scene items
-      (.add scene cube)
-      (.add scene cube2)
-      (.add scene cube3)
-      (set! (.. cube2 -position -x) 2)
-      (set! (.. cube2 -rotation -x) 0.5)
-      (set! (.. cube3 -position -x) -2)
-      (set! (.. cube3 -rotation -x) -0.5)
-      ; fly controls
-      ; (set! (.-movementSpeed controls) 5)
-      ; (set! (.-domElement controls) (.-domElement renderer))
-      ; (set! (.-autoForward controls) false)
-      ; (set! (.-dragToLook controls) false)
-      ; orbit controls
-      (.set (.-target controls) 0 0 0)
-      ; rest of actions
-      (set! (.. camera -position -z) 5)
-      (.addEventListener js/window "resize" onWindowResize)
-      (.addEventListener js/window "mousemove" onPointerMove)
-      ; necessary to convert to JSX
-      (r/as-element [:<>]))))
+    (.setPixelRatio renderer (.-devicePixelRatio js/window)) ; possibly not needed
+    (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
+    (.appendChild (.-body js/document) (.-domElement renderer))
+    ; add scene items
+    (.add scene cube)
+    (.add scene cube2)
+    (.add scene cube3)
+    (set! (.. cube2 -position -x) 2)
+    (set! (.. cube2 -rotation -x) 0.5)
+    (set! (.. cube3 -position -x) -2)
+    (set! (.. cube3 -rotation -x) -0.5)
+    ; fly controls
+    ; (set! (.-movementSpeed controls) 5)
+    ; (set! (.-domElement controls) (.-domElement renderer))
+    ; (set! (.-autoForward controls) false)
+    ; (set! (.-dragToLook controls) false)
+    ; orbit controls
+    (.set (.-target controls) 0 0 0)
+    ; rest of actions
+    (set! (.. camera -position -z) 5)
+    (.addEventListener js/window "resize" (onWindowResize renderer camera))
+    (.addEventListener js/window "mousemove" (onPointerMove pointer))
+    ; necessary to convert to JSX
+    (r/as-element [:<>])))
 
 ;; -------------------------
 ;; Views
